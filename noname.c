968552688ce4ca5bc8ec33e932dff49a4954033b
Sohail Shaik <47786532+infernexio@users.noreply.github.com>
2022-12-06 11:34:57 -0500

    had mistakes in the file and fixed them now works

--- noname.c
+++ noname.c
@@ -8,15 +8,15 @@ MODULE_DESCRIPTION("rootkit");
 MODULE_VERSION("0.0.1");
 
 static int __init init_func(void){
-    printk(KERN_INFO "rootkit: initalized");
+    printk(KERN_INFO "rootkit: initalized\n");
 
     return 0;
 }
 
 static void __exit exit_func(void){
-    printk(KERN_INFO " rootkit: stoped");
+    printk(KERN_INFO "rootkit: stoped\n");
 }
 
 
-module_init(mod_init);
-module_exit(mod_exit);
+module_init(init_func);
+module_exit(exit_func);
